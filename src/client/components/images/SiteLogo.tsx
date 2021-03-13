5e0fcf1ad15f00f25cd825fe05df7dcca15ef3e7
Mark Wilkinson <markwilko22@gmail.com>
2021-03-13 12:44:44 +0000

    Removed extPaths and fully integrated bucketPaths

--- src/client/components/images/SiteLogo.tsx
+++ src/client/components/images/SiteLogo.tsx
@@ -1,21 +1,22 @@
 //Modules
 import React from "react";
+import { connect, ConnectedProps } from "react-redux";
 
-//Constants
-import { imagePath } from "~/constants/extPaths";
-
-//Interfaces
-interface IProps {
+//Redux
+import { StoreState } from "~/client/reducers";
+interface IProps extends ConnectedProps<typeof connector> {
 	withShadow?: boolean;
 }
 
-export function SiteLogo({ withShadow }: IProps) {
-	let url = `${imagePath}layout/`;
-	if (withShadow) {
-		url += "logo_with_shadow.png";
-	} else {
-		url += "logo.png";
-	}
+function mapStateToProps({ config }: StoreState) {
+	const { bucketPaths } = config;
+	return { bucketPaths };
+}
+const connector = connect(mapStateToProps);
 
+//Component
+function _SiteLogo({ bucketPaths, withShadow }: IProps) {
+	const url = `${bucketPaths.images.layout}${withShadow ? "logo_with_shadow" : "logo"}.png`;
 	return <img className="site-logo" src={url} alt="Super League Pod Logo" />;
 }
+export const SiteLogo = connector(_SiteLogo);
